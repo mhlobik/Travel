848d40a96c3d6b613cb85a9c40839ef85e31972a
Marija <marija.hlobik@gmail.com>
2019-01-04 21:45:41 +0100

    delete

--- Travel.Frontend/src/components/city/cityPointsOfInterests.tsx
+++ Travel.Frontend/src/components/city/cityPointsOfInterests.tsx
@@ -14,7 +14,6 @@ export default class CityPointsOfInterests extends React.PureComponent<ICityPoin
         return (
             <div className="city-points-of-interests__container">
                 <img
-                    src={'https://maps.googleapis.com/maps/api/place/photo?photoreference=CmRaAAAAaDf2diztTfrOwWYoVecGqqDnLsrta90c069MQn9ftn4B-PFzS_M2yfhxOHEWn9lbrHoydjmLJC9Jbxd7v3NNwpzsI1EKkv02xJQCYKqrGPtH4QI94eIneK3DjbYKd5DyEhBALs_oJmrZngP8ktBRAcesGhSwpGTZy4uXCdL14kOprMhODPpj5g&sensor=false&maxheight=1960&maxwidth=4032&key=AIzaSyCsvdgvlSjZbK0NWLDA-lqi8pTTi5Ux_mw'}
                      ></img>
             </div>
         );
